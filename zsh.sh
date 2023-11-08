4640b40c5a5fc3af2d736900231a0e86c8e9568d
Gregory Duchatelet <gregory.duchatelet@infomaniak.com>
2023-11-08 09:51:24 +0100

    use native zsh if available

--- zsh.sh
+++ zsh.sh
@@ -26,9 +26,10 @@ export SAVEHIST=10000
 export HISTFILE=$XDG_DATA_HOME/zsh/history
 
 
-if [ -e /bin/zsh ]
-then
-    /bin/zsh "$@"
-else
-    $CDIR/$zshbin/bin/zsh "$@"
-fi
+#if [ -x /bin/zsh ]
+#then
+#    /bin/zsh "$@"
+#else
+#    $CDIR/$zshbin/bin/zsh "$@"
+#fi
+$CDIR/$zshbin/bin/zsh "$@"
